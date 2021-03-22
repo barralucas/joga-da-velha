b114c787d4b9d3004fa84c369196d1a816bd9e6c
Lucas Barra <lucastbarra@hotmail.com>
2021-03-22 18:52:12 -0300

    finalizando o projeto

--- scripts/game.js
+++ scripts/game.js
@@ -2,11 +2,20 @@
 let board = ["", "", "", "", "", "", "", "", ""];
 let playerTime = 0;
 let symbols = ["o", "x"];
+let gameOver = false;
 
 function handleMove(position) {
 
+
+    if (gameOver) {
+        return;
+    }
+
+
     if (board[position] == "") {
         board[position] = symbols[playerTime];
+
+        gameOver = isWin();
 
         if (playerTime == 0) {
             playerTime = 1;
@@ -16,3 +25,25 @@ function handleMove(position) {
     }
 }
 
+function isWin() {
+
+    let winStates = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winStates.length; i++) {
+        let seq = winStates[i];
+
+        let position1 = seq[0];
+        let position2 = seq[1];
+        let position3 = seq[2];
+    }
+
+}
